79d8e0b3179d22683914b467590ce6da225a21bd
AGawas <arnavgawas707@gmail.com>
2025-12-25 03:53:41 -0600

    IT WORKS!!! WOOHOO!!!

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
     libGL
     SDL2
     openal
+    icu
     xorg.libX11
     xorg.libXcursor
     xorg.libXi
@@ -22,23 +23,29 @@ pkgs.mkShell {
       pkgs.SDL2
       pkgs.openal
       pkgs.freetype
+      pkgs.libpng
+      pkgs.icu
       pkgs.xorg.libX11
       pkgs.xorg.libXcursor
       pkgs.xorg.libXi
       pkgs.xorg.libXrandr
     ]}:$LD_LIBRARY_PATH"
 
+    # export for font setup (failsafe for freetype, partially)
     export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
 
-    #failsafe
-    export LIBGL_ALWAYS_INDIRECT=1
+    # failsafe for if OpenGL is a bitch
+    export LIBGL_ALWAYS_SOFTWARE=1
+    export SDL_VIDEODRIVER=x11
 
-    # this is cursed asf ik
+    # cursed symlinking, im sorry but it works
     mkdir -p .lib-scripts
     ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/libfreetype6.so
-    ln -sf ${pkgs.libpng}/lib/libpng16.so .lib-scripts/libpng16.so
+    ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/freetype6.so
+    ln -sf ${pkgs.libpng}/lib/libpng16.so .lib-scripts/libpng16.so.16
+    
     export LD_LIBRARY_PATH="$(pwd)/.lib-scripts:$LD_LIBRARY_PATH"
 
-    echo "environment loaded!"
+    echo "environment loaded..."
   '';
 }
