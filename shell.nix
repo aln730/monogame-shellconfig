9b18ea906a3d55c35632c802d51410cab1311bc0
AGawas <arnavgawas707@gmail.com>
2025-12-25 02:22:55 -0500

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,6 +5,7 @@ pkgs.mkShell {
     dotnet-sdk_8
     fontconfig
     freetype
+    libpng      
     libGL
     xorg.libX11
     xorg.libXcursor
@@ -13,14 +14,21 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
-    export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ 
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ 
       pkgs.fontconfig 
       pkgs.freetype 
+      pkgs.libpng 
       pkgs.libGL 
       pkgs.xorg.libX11 
-    ]}:$LD_LIBRARY_PATH
+    ]}"
+    #I know thatthis is fcking cursed.
+    mkdir -p .lib-scripts
+    ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/libfreetype6.so
+    ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/freetype6.so
+    export LD_LIBRARY_PATH="$(pwd)/.lib-scripts:$LD_LIBRARY_PATH"
+
+    export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
     
-    echo "environment loaded"
+    echo "environment loaded!"
   '';
 }
