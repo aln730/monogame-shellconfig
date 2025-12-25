31c4e8d1bc81d7cdab7de3f6ed563ae76be5766c
AGawas <arnavgawas707@gmail.com>
2025-12-25 01:39:29 -0600

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,30 +5,34 @@ pkgs.mkShell {
     dotnet-sdk_8
     fontconfig
     freetype
-    libpng      
+    libpng
     libGL
+    SDL2
+    openal
     xorg.libX11
     xorg.libXcursor
     xorg.libXi
     xorg.libXrandr
+    gdb
   ];
 
   shellHook = ''
-    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ 
-      pkgs.fontconfig 
-      pkgs.freetype 
-      pkgs.libpng 
-      pkgs.libGL 
-      pkgs.xorg.libX11 
-    ]}"
-    #I know thatthis is fcking cursed.
-    mkdir -p .lib-scripts
-    ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/libfreetype6.so
-    ln -sf ${pkgs.freetype}/lib/libfreetype.so .lib-scripts/freetype6.so
-    export LD_LIBRARY_PATH="$(pwd)/.lib-scripts:$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib:${pkgs.lib.makeLibraryPath [
+      pkgs.libGL
+      pkgs.SDL2
+      pkgs.openal
+      pkgs.freetype
+      pkgs.xorg.libX11
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXi
+      pkgs.xorg.libXrandr
+    ]}:$LD_LIBRARY_PATH"
 
     export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
-    
+
+    #failsafe
+    export LIBGL_ALWAYS_INDIRECT=1
+
     echo "environment loaded!"
   '';
 }
